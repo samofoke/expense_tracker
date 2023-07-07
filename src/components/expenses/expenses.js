cde8f371fa53435441983e9cbedf3c337d504345
samofoke <sabataernestmofokeng@gmail.com>
2023-07-07 12:20:24 +0200

    more componets

--- src/components/expenses/expenses.js
+++ src/components/expenses/expenses.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import ExpenseItem from "../expense-item/Expense_item";
+// import ExpenseItem from "../expense-item/Expense_item";
 import ExpenseFilter from "../expensefilter/expense_filter";
 import Card from "../card/card";
 import "./expenses.css";
+import ExpenseList from "../expenselist/expense_list";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("2020");
@@ -15,18 +16,18 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filterYear;
   });
 
-  let expenseContent = <p>No expenses found</p>;
+  // let expenseContent = <p>No expenses found</p>;
 
-  if (filterExpense.length > 0) {
-    expenseContent = filterExpense.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
+  // if (filterExpense.length > 0) {
+  //   expenseContent = filterExpense.map((expense) => (
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ));
+  // }
 
   return (
     <Card className="expenses">
@@ -34,7 +35,7 @@ const Expenses = (props) => {
         selected={filterYear}
         theChangeFilter={filterChangeHandler}
       />
-      {expenseContent}
+      <ExpenseList items={filterExpense} />
     </Card>
   );
 };
